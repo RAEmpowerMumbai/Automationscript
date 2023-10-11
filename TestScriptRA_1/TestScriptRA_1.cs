6aa7f867866b4355495d7258174b8f73152fe8e3
amarrajesh <amarrajesh@gmail.com>
2023-10-11 16:54:34 +0530

    commit2

--- TestScriptRA_1/TestScriptRA_1.cs
+++ TestScriptRA_1/TestScriptRA_1.cs
@@ -17,7 +17,7 @@ namespace TestScriptRA_1
 		/// <param name="engine">Link with SLAutomation process.</param>
 		public void Run(IEngine engine)
 		{
-            engine.GenerateInformation("Hello universe! Hello");
+            engine.GenerateInformation("Hello universe!");
         }
 	}
 }
